79046310a8e1154bf5dbb4d6f9f20e33b2601441
liu kang <party_abu@163.com>
2022-05-10 21:41:37 +0800

    测试拦截器

---
+++ src/main/java/com/abucloud/interceptor/LoginInterceptor.java
@@ -0,0 +1,32 @@
+package com.abucloud.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Description:
+ * @Author party-abu
+ * @Date 2022/5/10 21:32
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("preHandler01");
+        // return true;
+        throw new RuntimeException();
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("afterCompletion01");
+    }
+}
